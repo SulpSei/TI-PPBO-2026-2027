8d9ce154bbd953ced612800d76e23954543ada7a
Sulti Syawal <sultisyawal07@gmail.com>
2026-09-13 02:01:25 +0700

    Praktikum 2: Komentar Kode A.3.

--- praktikum-2/src/HelloWorld.java
+++ praktikum-2/src/HelloWorld.java
@@ -6,5 +6,10 @@ public class HelloWorld {
         System.out.print("Sulti Syawal");
         System.out.println();
         System.out.println("Ini baris baru");
+
+        // Program menampilkan biodata sederhana
+        System.out.println("Nama: Budi");
+        /* Baris di bawah ini mencetak NIM mahasiswa */
+        System.out.println("NIM: 2025573010028");
     }
 }
