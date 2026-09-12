8ce413ad7a41e8630471533b74713bc3f2f5d642
Sulti Syawal <sultisyawal07@gmail.com>
2026-09-13 02:25:24 +0700

    Praktikum 2: Latihan NO 2 (Sesudah Di Modifikasi)

--- praktikum-2/src/BiodataSaya.java
+++ praktikum-2/src/BiodataSaya.java
@@ -1,7 +1,7 @@
 public class BiodataSaya {
     public static void main(String[] args){
         //Kode Untuk Menampilkan Nama
-        System.out.println("Nama: Sulti Syawal");
+        System.out.print("Nama: Sulti Syawal - ");
         //Kode Untuk Menampilkan Nim
         System.out.println("Nim: 2025573010028");
         //Kode Untuk Menampilkan Program Studi
