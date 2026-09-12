c40c60ffcea93457015ef3920196c69b4c583b0c
Sulti Syawal <sultisyawal07@gmail.com>
2026-09-13 01:59:18 +0700

    Praktikum 2: Struktur Dasar Program Java A.2.

--- praktikum-2/src/HelloWorld.java
+++ praktikum-2/src/HelloWorld.java
@@ -1,5 +1,10 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Halo Sulti Syawal");
+
+        System.out.print("Nama: ");
+        System.out.print("Sulti Syawal");
+        System.out.println();
+        System.out.println("Ini baris baru");
     }
 }
